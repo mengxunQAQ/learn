6bdc9fbe5326f6953e71e801758b6d4b27a4d88e
孟勋 <mengxun1122@163.com>
2018-09-27 10:29:14 +0800

    redis

--- redis/drift_bottle.py
+++ redis/drift_bottle.py
@@ -3,39 +3,44 @@ import redis
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-class Drift:
-    def __init__(self):
-        self.count_hash = "count"
-        self.user_hash = "user"
-        
-    def throw(self, user, msg):
-        
-        key = user + ":throw"
-    
-        if r.hget(self.count_hash, key) != 10:
-            r.hincrby(self.count_hash, key)
-            count = r.hget(self.count_hash, key)
-            msg_hash = user + ":{count}".format(count=count)
-            r.sadd(self.user_hash, msg_hash)
-            r.hmset("msg", {msg_hash:msg})
-    
-    
-    def pick(self, user):
-        key = user + ":pick"
-    
-        if r.hget(self.count_hash, key) != 10:
-            r.hincrby(self.count_hash, key)
-            msg_hash = r.srandmember(self.user_hash, 1)
-            result = r.hget("msg", msg_hash[0])
-        else:
-            result = "Today's opportunity is gone!"
-            return result
+
+def throw(user, msg):
+    if r.scard(user) < 10:
+        r.sadd(user, msg)
+        r.sadd("members", user)
+        print("瓶子已经扔向大海...")
+    else:
+        print("今天扔瓶子机会已经用完~~~明天再来吧")
+
+
+def pick(user):
+    key = user+":count"
+    r.incr(key)
+    pick_count = int(r.get(key))
+    if  pick_count < 10:
+        member = r.srandmember("members", number=1)[0]
+        msg = str(r.spop(member))
+        print(msg)
+    else:
+        print("今天捡瓶子机会已经用完~~~明天再来吧")
 
 
 if __name__ == '__main__':
-    drift = Drift()
-    drift.throw("Alen", "good night!")
-    result = drift.pick("Bob")
-    print(result)
+        choice = input("捡瓶子还是瓶子？1-扔瓶子 2-捡瓶子：")
+        if choice == "1":
+            user = input("输入您的用户名：")
+            while True:
+                msg = input("说点想说的：")
+                throw(user, msg)
+                option = input("是否接着扔（y/n）：")
+                if option != "y":
+                    break
 
+        elif choice == "2":
+            user = input("输入您的用户名：")
+            while True:
+                pick(user)
+                option = input("是否接着捡（y/n）：")
+                if option != "y":
+                    break
 
