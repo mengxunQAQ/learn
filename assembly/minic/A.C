ff82055abaf596139ca588df1eaa7f514c759c31
孟勋 <mengxun1122@163.com>
2019-01-10 18:36:31 +0800

    add c file

---
+++ assembly/minic/A.C
@@ -0,0 +1,12 @@
+void showchar(char a,int b);
+
+main()
+{
+ showchar('a',2);
+}
+
+void showchar(char a,int b)
+{
+ *(char far *)(0x8000000+160*10+80)=a;
+ *(char far *)(0x8000000+160*10+81)=b;
+}
