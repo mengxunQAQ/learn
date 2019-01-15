0229dc7d8d946cfc95ce1d248b23e3d566bd5457
mengxun <mengxun1122@163.com>
2019-01-15 22:50:55 +0800

    add example 3.1

---
+++ c/c_primer_plus/3/3_1.c
@@ -0,0 +1,22 @@
+/*换算体重到白金价值*/
+#include <stdio.h>
+
+int main(void)
+{
+    float weight; //你的体重
+    float value;  //相同重量到白金价值
+
+    printf("Are you worth your weight in platinum?\n");
+    printf("Let's check it out.\n");
+    printf("Please enter your weight in pounds: ");
+    //获取用户输入
+    scanf("%f", &weight);
+    //假设白金价格每盎司1700美金
+    //14.5833用于把英镑常衡盎司转换为金衡盎司
+    value = 1700.0*weight*14.5833;
+    printf("Your weight in platinum is worth $%.2f.\n", value);
+    printf("You are easily worth that! If platinum prices drop,\n");
+    printf("eat more to maintain your value.\n");
+    return 0;
+}
+
