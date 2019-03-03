6ee226115317aff3393f5dc35ab27cbdf1541efe
mengxun <mengxun1122@163.com>
2019-03-03 23:01:06 +0800

    add 5.7

---
+++ c/c_primer_plus/05/07.c
@@ -0,0 +1,10 @@
+/*优先级测试*/
+#include <stdio.h>
+
+int main(void)
+{
+    int top, score;
+    top = score = -(2+5)*6 + (4+3*(2+3));
+    printf("top = %d, socre = %d\n", top, score);
+    return 0;
+}
