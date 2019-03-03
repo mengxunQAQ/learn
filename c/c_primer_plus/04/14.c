fc5179c1160ffcc9a88a9a29265dd17971a7669c
mengxun <mengxun1122@163.com>
2019-03-03 15:42:09 +0800

    add 4.14

---
+++ c/c_primer_plus/04/14.c
@@ -0,0 +1,13 @@
+/*打印较长的字符串*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Here's one way to print a ");
+    printf("long string.\n");
+    printf("Here's anther way to print a \
+long string.\n");
+    printf("Here's the newest way to print a"
+            "long string.\n");
+    return 0;
+}
