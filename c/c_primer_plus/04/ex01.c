3283adafd9d7fda5cb8fffa347ee702e2e4d767c
mengxun <mengxun1122@163.com>
2019-03-03 17:33:05 +0800

    add ex01

---
+++ c/c_primer_plus/04/ex01.c
@@ -0,0 +1,11 @@
+//打印姓名
+#include <stdio.h>
+
+int main(void)
+{
+    char name1[20], name2[20];
+    printf("enter your name:\n");
+    scanf("%s %s", name1, name2);
+    printf("%s,%s\n", name1, name2);
+    return 0;
+}
