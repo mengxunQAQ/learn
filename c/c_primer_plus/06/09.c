c89ce1860884a7395d1a48241aee63eb68304cb5
mengxun <mengxun1122@163.com>
2019-03-06 23:43:33 +0800

    add 6.9

---
+++ c/c_primer_plus/06/09.c
@@ -0,0 +1,20 @@
+/*误用=会导致无限循环*/
+#include <stdio.h>
+
+int main(void){
+    long num;
+    long sum = 0L;
+    _Bool input_is_good;
+ 
+    printf("Please enter an interger to be summed ");
+    printf("(q to quit): ");
+    input_is_good = (scanf("%ld", &num) == 1);
+    while (input_is_good){
+        sum = sum + num;
+        printf("Please enter next integer (q to quit): ");
+        input_is_good = (scanf("%ld", &num) == 1);
+    }
+    printf("Those integers sum to %ld.\n", sum);
+
+    return 0;
+}
