5ed59cafde9af7d639cc8ba1556d595be8f49e6d
mengxun <mengxun1122@163.com>
2019-03-09 22:58:02 +0800

    add 7.11

---
+++ c/c_primer_plus/07/11.c
@@ -0,0 +1,54 @@
+/*使用switch语句*/
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char ch;
+
+    printf("Give me a letter of the alphabet, and I will give ");
+    printf("an animal name\nbeginning with that letter.\n");
+    printf("Please type in a letter; type # to end my act.\n");
+
+    while ((ch = getchar()) != '#')
+    {
+        if ('\n' == ch)
+            continue;
+        
+        if (islower(ch))
+            switch (ch)
+            {
+                case 'a':
+                    printf("argali, a wild sheep of Asia\n");
+                    break;
+                case 'b':
+                    printf("babirusa, a wild pig of Malay\n");
+                    break;
+                case 'c':
+                    printf("coati, racoonlike mammal\n");
+                    break;
+                case 'd':
+                    printf("desman, aquatic, molelike critter\n");
+                    break;
+                case 'e':
+                    printf("echidna, the spiny anteater\n");
+                    break;
+                case 'f':
+                    printf("fisher, brownish marten\n");
+                    break;
+                default:
+                    printf("That's a stumper!\n");
+            }  /*switch结束*/
+        else
+            printf("I recognize only lowercase letters.\n");
+        
+        while (getchar() != '\n')
+            continue;
+
+        printf("Plases type another letter or a #.\n");
+    }  /*end while*/
+
+    printf("Bye!\n");
+
+    return 0;
+}
