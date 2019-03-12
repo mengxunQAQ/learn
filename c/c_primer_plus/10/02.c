d3f2d4634eb3bd6e8442d040fb8c236ec34eb2f2
mengxun <mengxun1122@163.com>
2019-03-12 23:58:35 +0800

    add 10.2

---
+++ c/c_primer_plus/10/02.c
@@ -0,0 +1,17 @@
+/* 未初始化数组 */
+#include <stdio.h>
+#define SIZE 4
+
+int main(void)
+{
+    int no_data[SIZE];  // not init
+    int i;
+    
+    printf("%2s%14s\n", "i", "no_data[i]");
+
+    for (i = 0; i < SIZE; i++)
+        printf("%2d%14d\n", i, no_data[i]);
+
+    return 0;
+}
+
