3a6dc765639ef9ae98c605d27092e205032511b5
mengxun <mengxun1122@163.com>
2019-03-12 23:58:54 +0800

    add 10.4

---
+++ c/c_primer_plus/10/04.c
@@ -0,0 +1,16 @@
+/*10.4 自动匹配项数*/
+#include <stdio.h>
+
+int main(void)
+{
+    const int days[] = {31, 28, 31, 30, 31, 30, 31, 31,
+                        30, 31};
+    int index;
+
+    for (index = 0; index < sizeof days / sizeof days[0];
+            index++)
+        printf("Month %2d has %d days.\n", index + 1, days[index]);
+
+    return 0;
+}
+
