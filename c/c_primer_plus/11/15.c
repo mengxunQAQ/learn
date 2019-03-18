a5169195e1340137d9cd42c259a6f7f3f226988e
mengxun <mengxun1122@163.com>
2019-03-18 15:50:38 +0800

    add 11.15

---
+++ c/c_primer_plus/11/15.c
@@ -0,0 +1,17 @@
+/* 打印一个字符串，并统计打印的字符数 */
+#include <stdio.h>
+
+int put2(const char *string)
+{
+    int count = 0;
+
+    while (*string)  /* 常规用法 */
+    {
+        putchar(*string++);
+        count++;
+    }
+
+    putchar('\n');
+
+    return(count);
+}
