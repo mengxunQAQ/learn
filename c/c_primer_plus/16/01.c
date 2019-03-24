8d410ed5082adbb9265228be225792b81281686c
mengxun <mengxun1122@163.com>
2019-03-24 17:57:43 +0800

    add 16.1

---
+++ c/c_primer_plus/16/01.c
@@ -0,0 +1,21 @@
+/* 预处理 */
+#include <stdio.h>
+#define TWO 2  /* comment */
+#define OW "Consistency is the last refuge of the unimagina\
+    tibe.-Oscar Wilde"
+#define FOUR TWO*TWO
+#define PX printf("X is %d.\n", x)
+#define FMT "X is %d.\n"
+
+int main(void)
+{
+    int x = TWO;
+    PX;
+    x = FOUR;
+ 
+    printf(FMT, x);
+    printf("%s\n", OW);
+    printf("TWO: OW\n");
+
+    return 0;
+}
