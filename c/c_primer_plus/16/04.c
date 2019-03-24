91be32c736fb74a53399999309cb182b7826b5dc
mengxun <mengxun1122@163.com>
2019-03-24 18:22:10 +0800

    add 16.4

---
+++ c/c_primer_plus/16/04.c
@@ -0,0 +1,18 @@
+// 使用##运算符
+#include <stdio.h>
+#define XNAME(n) x ## n
+#define PRINT_XN(n) printf("x"#n" = %d\n", x ## n);
+
+int main(void)
+{
+    int XNAME(1) = 14;  // int x1 = 14;
+    int XNAME(2) = 20;  // int x2 = 20;
+    int x3 = 30;
+
+    PRINT_XN(1);  // printf("x1 = %d\n", x1);
+    PRINT_XN(2);
+    PRINT_XN(3);
+    
+    return 0;
+}
+
