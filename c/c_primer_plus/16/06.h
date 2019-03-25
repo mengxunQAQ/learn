ec9f67860586c6204f0e0ced36c23a0e1c6057cf
mengxun <mengxun1122@163.com>
2019-03-25 18:54:14 +0800

    add 16.6

---
+++ c/c_primer_plus/16/06.h
@@ -0,0 +1,20 @@
+// names_st 结构的头文件
+#include <string.h>
+#define SLEN 32  // 常量
+
+//结构声明
+struct names_st
+{
+    char first[SLEN];
+    char last[SLEN];
+};
+
+//类型定义
+typedef struct name_st names;
+
+//函数原型
+void get_names(names *);
+void show_names(const names *);
+char *s_gets(char *st, int n);
+
+
