0133580e302fd6f18329f6fc8f6bfa93eb21701a
mengxun <mengxun1122@163.com>
2019-03-25 22:59:45 +0800

    fix 16.7

--- c/c_primer_plus/16/07.c
+++ c/c_primer_plus/16/07.c
@@ -2,4 +2,37 @@
 #include <stdio.h>
 #include "06.h"  // 包含头文件
 
-void get_names
+void get_names(names *pn)
+{
+    printf("Please enter your first name: ");
+    s_gets(pn -> first, SLEN);
+    printf("Please enter your last name: ");
+    s_gets(pn -> last, SLEN);
+}
+
+void show_names(const names *pn)
+{
+    printf("%s %s", pn -> first, pn -> last);
+}
+
+char *s_gets(char *st, int n)
+{
+    char *ret_val;
+    char *find;
+
+    ret_val = fgets(st, n,  stdin);
+
+    if (ret_val)
+    {
+        find = strchr(st, '\n');  // 查找换行符
+
+        if (find)
+            *find = '\0';
+        else
+            while (getchar() != '\n')
+                continue;
+    }
+
+    return ret_val;
+}
+
