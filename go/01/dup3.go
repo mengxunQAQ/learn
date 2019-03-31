4494897ede8f6376db11fff7ac15dd61aa1974f0
mengxun <mengxun1122@163.com>
2019-03-31 20:27:14 +0800

    fix dup3

--- go/01/dup3.go
+++ go/01/dup3.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[sting]int)
+
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+
