24e8dec09acb1d8fbeee8ab19c31504890c10e0e
mengxun <mengxun1122@163.com>
2019-04-13 22:27:34 +0800

    iota

--- go/basic/ex02.go
+++ go/basic/ex02.go
@@ -10,11 +10,11 @@ func main() {
 	)
 	fmt.Printf("a = %d b = %d\n", a, b)
 
-	const c = iota
+	const c = iota // 遇见const就会置0
 	fmt.Printf("c = %d\n", c)
 
 	const (
-		d = iota
+		d = iota // 后面可以不写iota
 		e
 		f
 	)
@@ -22,7 +22,7 @@ func main() {
 
 	const (
 		j          = iota
-		h1, h2, h3 = iota, iota, iota
+		h1, h2, h3 = iota, iota, iota // 三个变量的值都一样
 		i          = iota
 	)
 	fmt.Printf("j = %d h1 = %d h2 = %d h3 = %d i = %d\n", j, h1, h2, h3, i)
