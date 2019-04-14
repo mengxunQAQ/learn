ae89e39dd23989af52ba4bcc1c9bd3a14c998348
mengxun <mengxun1122@163.com>
2019-04-14 21:40:43 +0800

    switch

--- go/basic/ex05.go
+++ go/basic/ex05.go
@@ -13,10 +13,30 @@ func main() {
 		fallthrough               // 相当于c不加break
 	case 2:
 		fmt.Println("enter 2...")
-	case 3:
-		fmt.Println("enter 3...")
+	case 3, 4: // 多个条件放一行
+		fmt.Println("enter 3,4...")
 	default:
 		fmt.Println("out of range!")
 
+	}
+
+	a := 2
+
+	switch { // 没有条件
+	case a > 2: // 条件放在case后面
+		fmt.Println("a > 2")
+	case a > 1:
+		fmt.Println("a > 1")
+	default:
+		fmt.Println("a <= 1")
+	}
+
+	switch b := 3; b { // 初始化
+	case 3:
+		fmt.Println("b = 3")
+	case 2:
+		fmt.Println("b = 2")
+	default:
+		fmt.Println("b = xxx")
 	}
 }
