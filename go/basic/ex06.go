2e949560050989f685f205963696ce4b422abc3b
mengxun <mengxun1122@163.com>
2019-04-14 22:18:39 +0800

    for & range

---
+++ go/basic/ex06.go
@@ -0,0 +1,24 @@
+/* for循环 和 range */
+package main
+
+import "fmt"
+
+func main() {
+	str := "abc"
+
+	for i := 0; i < len(str); i++ {
+		fmt.Printf("str[%d] = %c\n", i, str[i])
+	}
+
+	for i, data := range str {
+		fmt.Printf("str[%d] = %c\n", i, data)
+	}
+
+	for _, data := range str {
+		fmt.Printf("%c\n", data)
+	}
+
+	for i := range str {
+		fmt.Printf("str[%d] = %c\n", i, str[i])
+	}
+}
