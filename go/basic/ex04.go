453f61225b0e6b0c99639d3e756336a97b601c5d
mengxun <mengxun1122@163.com>
2019-04-14 14:56:52 +0800

    if

---
+++ go/basic/ex04.go
@@ -0,0 +1,16 @@
+/* if判断 */
+package main
+
+import "fmt"
+
+func main() {
+	a := 1
+
+	if a == 1 {
+		fmt.Println(a)
+	}
+
+	if b := 1; b == 1 { // if支持带一个初始化语句，初始化语句和判断语句以分号分割
+		fmt.Println(b)
+	}
+}
