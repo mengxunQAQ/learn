c1e34a8821cc53e5e07af048a1bb281d72b12302
mengxun <mengxun1122@163.com>
2019-04-21 23:10:04 +0800

    defer

--- go/basic/02/ex06.go
+++ go/basic/02/ex06.go
@@ -5,6 +5,7 @@ import "fmt"
 
 func main() {
 	fmt.Println(1)
-	defer fmt.Println(2)
-	fmt.Println(3)
+	defer fmt.Println(2) // 多个defer先声明的后调用
+	defer fmt.Println(3) // 无论发生何种错误，defer总是能被调用
+	fmt.Println(4)
 }
