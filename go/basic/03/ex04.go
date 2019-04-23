36a1f297b5f0f9c8fd3d8cd305ea95b9e69a89a4
mengxun <mengxun1122@163.com>
2019-04-23 22:58:25 +0800

    array

--- go/basic/03/ex04.go
+++ go/basic/03/ex04.go
@@ -6,8 +6,8 @@ import "fmt"
 func main() {
 	var id [50]int // go里面未初始化的变量，值默认是0
 
-	for i, data := range id {
+	for i := range id {
 		id[i] = i + 1
-		fmt.Println(data, id[i])
+		fmt.Println(id[i])
 	}
 }
