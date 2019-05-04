9c3e3574260b7545c1d3572b8ae7b29545b5d966
mengxun <mengxun1122@163.com>
2019-05-04 20:01:30 +0800

    fetchall

---
+++ go/the_go_programming_language/01/fetchall.go
@@ -0,0 +1,48 @@
+/*
+多个url请求
+* 利用 goroutine 进行并发
+* 利用 channel 进行 goroutine 之间的同步
+*/
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // 从管道接受数据，如果没数据则阻塞在这里
+	}
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
