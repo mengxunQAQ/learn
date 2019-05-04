a155188201fa51c7852c9e4c081c168965c27157
mengxun <mengxun1122@163.com>
2019-05-04 20:33:25 +0800

    server2

---
+++ go/the_go_programming_language/01/server2.go
@@ -0,0 +1,35 @@
+/*
+web服务器
+* 增加统计访问次数的功能
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	//	fmt.Println(count, r.URL.Path)
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.PATH = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
