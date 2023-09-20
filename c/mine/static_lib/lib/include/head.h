90fe3f07aa79681923140b3be92680e29de03482
mengxun <mengxun1122@163.com>
2023-09-21 00:56:48 +0800

    add static lib

---
+++ c/mine/static_lib/lib/include/head.h
@@ -0,0 +1,6 @@
+#ifndef _HEAD_H
+#define _HEAD_H
+
+int add(int, int);
+
+#endif
---
+++ c/mine/static_lib/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "head.h"
+
+int main(void) {
+    int i = 1;
+    int j = 2;
+    int sum = add(i, j);
+    printf("sum is %d\n", sum);
+}
+
+
+
